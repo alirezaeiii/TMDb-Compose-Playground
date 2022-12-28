f216d3e6fe23a2293520603e0408ef514a4ee0cf
Ali <ali.rezaei09@gmail.com>
2022-12-28 13:59:30 +0330

    Simplify ProgressBar

--- app/src/main/java/com/android/sample/tmdb/ui/feed/Content.kt
+++ app/src/main/java/com/android/sample/tmdb/ui/feed/Content.kt
@@ -2,11 +2,10 @@ package com.android.sample.tmdb.ui.feed
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import com.android.sample.tmdb.R
 import com.android.sample.tmdb.data.FeedWrapper
 import com.android.sample.tmdb.data.TMDbItem
 import com.android.sample.tmdb.ui.common.ErrorScreen
-import com.android.sample.tmdb.ui.common.LoopReverseLottieLoader
+import com.android.sample.tmdb.ui.common.TMDbProgressBar
 import com.android.sample.tmdb.utils.Resource
 
 @Composable
@@ -15,7 +14,7 @@ fun <T : TMDbItem> Content(
     SuccessScreen: @Composable (t: List<FeedWrapper<T>>) -> Unit
 ) {
     when (val resource = viewModel.stateFlow.collectAsState().value) {
-        is Resource.Loading -> LoopReverseLottieLoader(lottieFile = R.raw.loader)
+        is Resource.Loading -> TMDbProgressBar()
         is Resource.Success -> SuccessScreen(resource.data)
         is Resource.Error -> ErrorScreen(message = resource.message, refresh = viewModel::refresh)
     }
