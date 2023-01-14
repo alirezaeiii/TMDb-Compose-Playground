c42fa637e7ecc563a2a835b0c3eb0e0a3bb0fcd5
Ali <ali.rezaei09@gmail.com>
2023-01-14 22:27:36 +0330

    Add search icon

--- app/src/main/java/com/android/sample/tmdb/ui/common/DestinationBar.kt
+++ app/src/main/java/com/android/sample/tmdb/ui/common/DestinationBar.kt
@@ -4,15 +4,15 @@ import android.content.res.Configuration
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.ExpandMore
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.android.sample.tmdb.R
 import com.android.sample.tmdb.ui.theme.AlphaNearOpaque
 import com.android.sample.tmdb.ui.theme.TmdbPagingComposeTheme
 
@@ -40,7 +40,7 @@ fun DestinationBar(title: String, modifier: Modifier = Modifier) {
                 modifier = Modifier.align(Alignment.CenterVertically)
             ) {
                 Icon(
-                    imageVector = Icons.Outlined.ExpandMore,
+                    painter = painterResource(R.drawable.ic_search),
                     contentDescription = null
                 )
             }
