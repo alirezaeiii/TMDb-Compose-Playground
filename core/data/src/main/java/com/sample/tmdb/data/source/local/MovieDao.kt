aae2b3ef7ade18b0210fc0ef7f091e04ba01111a
Ali <ali.rezaei09@gmail.com>
2023-11-04 14:33:04 -0700

    Text modification

--- core/data/src/main/java/com/sample/tmdb/data/source/local/MovieDao.kt
+++ core/data/src/main/java/com/sample/tmdb/data/source/local/MovieDao.kt
@@ -12,12 +12,12 @@ interface MovieDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun addBookmark(movieEntity: MovieEntity)
 
-    @Query("DELETE FROM movies WHERE id = :id")
+    @Query("DELETE FROM Movies WHERE id = :id")
     suspend fun deleteBookmark(id: Int)
 
     @Query("SELECT EXISTS(SELECT * FROM Movies WHERE id = :id)")
     suspend fun isBookmarked(id: Int): Boolean
 
-    @Query("SELECT * FROM movies ORDER BY releaseDate DESC")
+    @Query("SELECT * FROM Movies ORDER BY releaseDate DESC")
     suspend fun getBookmarks(): List<MovieEntity>
 }
