d14c5fae953290224b4587b32d413c25666fd99d
Ali <ali.rezaei09@gmail.com>
2024-08-13 02:41:12 +0330

    Unit test for bookmark in detail screen

--- features/feature-detail/src/test/java/com/sample/tmdb/detail/TVShowDetailViewModelTest.kt
+++ features/feature-detail/src/test/java/com/sample/tmdb/detail/TVShowDetailViewModelTest.kt
@@ -3,9 +3,15 @@ package com.sample.tmdb.detail
 import com.sample.tmdb.domain.model.TVShow
 import com.sample.tmdb.domain.model.TvDetails
 
-class TVShowDetailViewModelTest: BaseDetailViewModelTest<TvDetails, TVShow>() {
+class TVShowDetailViewModelTest : BaseDetailViewModelTest<TvDetails, TVShow>() {
 
     override fun initViewModel() {
         super.viewModel = TVShowDetailViewModel(bookmarkRepository, repository, savedStateHandle)
     }
+
+    override val tmdbItem: TVShow
+        get() = TVShow(
+            TMDB_ITEM_ID, "overview", null, null, null,
+            "name", 1.0, 1
+        )
 }
