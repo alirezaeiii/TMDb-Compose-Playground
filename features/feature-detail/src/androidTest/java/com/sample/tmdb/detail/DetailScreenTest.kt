fc282253608c18c065e437be89e4daf31ccd94ab
Ali <ali.rezaei09@gmail.com>
2024-08-16 15:52:42 +0330

    Improve compose ui tests

--- features/feature-detail/src/androidTest/java/com/sample/tmdb/detail/DetailScreenTest.kt
+++ features/feature-detail/src/androidTest/java/com/sample/tmdb/detail/DetailScreenTest.kt
@@ -1,8 +1,10 @@
 package com.sample.tmdb.detail
 
+import android.annotation.SuppressLint
+import androidx.activity.ComponentActivity
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.test.assertIsDisplayed
-import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithText
 import com.sample.tmdb.domain.model.MovieDetails
@@ -13,11 +15,12 @@ import org.mockito.ArgumentMatchers.anyDouble
 import org.mockito.ArgumentMatchers.anyInt
 import org.mockito.ArgumentMatchers.anyList
 import org.mockito.ArgumentMatchers.anyString
+import com.sample.tmdb.common.R as commonR
 
 class DetailScreenTest {
 
     @get:Rule
-    val composeTestRule = createComposeRule()
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
 
     @Test
     fun posterTest() {
@@ -54,11 +57,11 @@ class DetailScreenTest {
                     )
                 )
             }
-            onNodeWithText("Release Date").assertIsDisplayed()
+            onNodeWithText(activity.getString(R.string.release_date)).assertIsDisplayed()
             onNodeWithText("2024-12-29").assertIsDisplayed()
-            onNodeWithText("Vote Avg").assertIsDisplayed()
+            onNodeWithText(activity.getString(R.string.vote_average)).assertIsDisplayed()
             onNodeWithText("1.0").assertIsDisplayed()
-            onNodeWithText("Votes").assertIsDisplayed()
+            onNodeWithText(activity.getString(R.string.votes)).assertIsDisplayed()
             onNodeWithText("1").assertIsDisplayed()
         }
     }
@@ -80,27 +83,30 @@ class DetailScreenTest {
             setContent {
                 ImageSection(image = TMDbImage(anyString(), anyInt())) {}
             }
-            onNodeWithContentDescription("Poster").assertIsDisplayed()
+            onNodeWithContentDescription(activity.getString(commonR.string.poster_content_description))
+                .assertIsDisplayed()
         }
     }
 
+    @SuppressLint("UnrememberedMutableState")
     @Test
     fun toggleBookmarkFabTest() {
         with(composeTestRule) {
             setContent {
                 ToggleBookmarkFab(isBookmark = true, isVisible = mutableStateOf(true)) {}
             }
-            onNodeWithContentDescription("Favorite").assertIsDisplayed()
+            onNodeWithContentDescription(activity.getString(R.string.favorite)).assertIsDisplayed()
         }
     }
 
+    @SuppressLint("UnrememberedMutableState")
     @Test
     fun toggleUnBookmarkFabTest() {
         with(composeTestRule) {
             setContent {
                 ToggleBookmarkFab(isBookmark = false, isVisible = mutableStateOf(true)) {}
             }
-            onNodeWithContentDescription("Un-favorite").assertIsDisplayed()
+            onNodeWithContentDescription(activity.getString(R.string.un_favorite)).assertIsDisplayed()
         }
     }
 }
