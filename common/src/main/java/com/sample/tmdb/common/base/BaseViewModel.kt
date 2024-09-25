6e44757e98c947ff3cb94ce630737e30d7cc39e5
Ali <ali.rezaei09@gmail.com>
2024-09-26 03:23:48 +0330

    Modification

--- common/src/main/java/com/sample/tmdb/common/base/BaseViewModel.kt
+++ common/src/main/java/com/sample/tmdb/common/base/BaseViewModel.kt
@@ -8,6 +8,7 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 
 open class BaseViewModel<T>(private val repository: BaseRepository<T>, private val id: Any? = null) : ViewModel() {
+
     private val _stateFlow = MutableStateFlow<Resource<T>>(Resource.Loading)
     val stateFlow: StateFlow<Resource<T>>
         get() = _stateFlow
