622f8ccb7d694b1f26c4da4393676dcca83a70f9
Ali <m.ali.rezaeiii09@gmail.com>
2025-08-17 15:10:43 +0330

    Modification

--- features/feature-credit/src/androidTest/java/com/sample/tmdb/credit/CreditScreenTest.kt
+++ features/feature-credit/src/androidTest/java/com/sample/tmdb/credit/CreditScreenTest.kt
@@ -7,12 +7,12 @@ import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithText
 import androidx.navigation.compose.rememberNavController
+import com.sample.tmdb.common.R as commonR
 import com.sample.tmdb.common.model.Gender
 import com.sample.tmdb.common.model.placeholderIcon
 import com.sample.tmdb.domain.model.Cast
 import org.junit.Rule
 import org.junit.Test
-import com.sample.tmdb.common.R as commonR
 
 class CreditScreenTest {
     @get:Rule
